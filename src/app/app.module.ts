b815e4f68093d607975628e052f2539911763dfd
Dulce Padrón <122685789+IDGS-802-19002099@users.noreply.github.com>
2023-06-15 20:58:53 -0600

    DJPV clase 15.06.2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppComponent } from './app.component';
 import { NamesGrupos } from './utl/grupos.component';
 import { NamesGrupos902 } from './utl/grupo902.component';
 import { IdgsComponent } from './utl/idgs/idgs.component';
+import { IricComponent } from './utl/iric/iric.component';
+import { MenuComponent } from './utl/menu/menu.component';
+import { FormsModule } from '@angular/forms';
+import { SumaComponent } from './utl/formularios/suma/suma.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OperasBasComponent } from './utl/formularios/operas-bas/operas-bas.component';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
     NamesGrupos,
     NamesGrupos902,
-    IdgsComponent
+    IdgsComponent,
+    IricComponent,
+    MenuComponent,
+    SumaComponent,
+    OperasBasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
